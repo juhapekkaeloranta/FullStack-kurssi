4fdf0509f4e272e6ac08a3b83b7648d2ba85db9b
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-01-21 15:51:47 +0200

    teht 1-9

--- Osa_1/one-six-unicafe/src/index.js
+++ Osa_1/one-six-unicafe/src/index.js
@@ -9,43 +9,81 @@ const Button = ({ handleClick, buttonText }) => {
   )
 }
 
+const Statistics = ({ statisticsData }) => {
+  if (statisticsData.countRatings > 0) {
+    return (
+    <div>
+      <h2>Statistiikka</h2>
+      <Statistic statName='hyvät' statData={statisticsData.good} />
+      <Statistic statName='neutraalit' statData={statisticsData.neutral} />
+      <Statistic statName='huonot' statData={statisticsData.bad} />
+      <Statistic statName='keskiarvo' statData={(statisticsData.sumRatings / statisticsData.countRatings).toFixed(1)} />
+      <Statistic statName='positiivisia' statData={(statisticsData.good / statisticsData.countRatings * 100).toFixed(0) + ' %'} />
+    </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Statistiikka</h2>
+        <p>Yhtään palautetta ei ole annettu</p>
+      </div>
+    )
+  }
+}
+
+const Statistic = ({ statName, statData }) => {
+  return (
+    <p>{statName}: {statData}</p>
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      hyvat: 0,
-      neutraalit: 0,
-      huonot: 0
+      good: 0,
+      neutral: 0,
+      bad: 0,
+      countRatings: 0,
+      sumRatings: 0,
     }
-    this.annaHyvaArvio = this.annaHyvaArvio.bind(this)
-    this.annaNeutraaliArvio = this.annaNeutraaliArvio.bind(this)
-    this.annaHuonoArvio = this.annaHuonoArvio.bind(this)
   }
 
-  annaHyvaArvio() {
-    this.setState({ hyvat: this.state.hyvat + 1})
+  giveGoodRating = () => {
+    this.setState({ good: this.state.good + 1})
+    this.addRatingCount()
+    this.updateTotalRating(1)
   }
 
-  annaNeutraaliArvio() {
-    this.setState({ neutraalit: this.state.neutraalit + 1})
+  giveNeutralRating = () => {
+    this.setState({ neutral: this.state.neutral + 1})
+    this.addRatingCount()
+    this.updateTotalRating(0)
   }
   
-  annaHuonoArvio() {
-    this.setState({ huonot: this.state.huonot + 1})
+  giveBadRating = () => {
+    this.setState({ bad: this.state.bad + 1})
+    this.addRatingCount()
+    this.updateTotalRating(-1)
   }
 
+  addRatingCount = () => {
+    this.setState({ countRatings: this.state.countRatings + 1})
+  }
+
+  updateTotalRating = (amount) => {
+    this.setState({ sumRatings: this.state.sumRatings + amount})
+  }
 
   render() {
     return (
       <div>
         <h1>Anna palautetta päivän ruoasta:</h1>
-        <Button buttonText='hyvä' handleClick={this.annaHyvaArvio}/>
-        <Button buttonText='neutraali' handleClick={this.annaNeutraaliArvio} />
-        <Button buttonText='huono' handleClick={this.annaHuonoArvio} />
-        <h2>Statistiikka</h2>
-        <div><p>hyvät: {this.state.hyvat}</p></div>
-        <div><p>neutraalit: {this.state.neutraalit}</p></div>
-        <div><p>huonot: {this.state.huonot}</p></div>
+        <Button buttonText='hyvä' handleClick={this.giveGoodRating}/>
+        <Button buttonText='neutraali' handleClick={this.giveNeutralRating} />
+        <Button buttonText='huono' handleClick={this.giveBadRating} />      
+        <Statistics statisticsData={this.state} />
+        <p>{this.countAvg}</p>
       </div>
     )
   }
