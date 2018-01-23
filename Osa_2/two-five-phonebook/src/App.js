9148f879af07dda8388770f86e055801f88e9e51
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-01-23 10:54:56 +0200

    teht 2-6 kesken

--- Osa_2/two-five-phonebook/src/App.js
+++ Osa_2/two-five-phonebook/src/App.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+const Row = ({ content }) => {
+  return (
+    <li key={content}>{content}</li>
+  )
+}
+
+const Rows = ({ rowData }) => {
+  return (
+    <ul>
+      {rowData.map(row => (<Row content={row.name}/>))}
+    </ul>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' },
+        { name: 'Aku Ankka'}
+      ],
+      newName: ''
+    }
+  }
+
+  addContact = (event) => {
+    event.preventDefault()
+    console.log('addContact called!')
+    console.log(event)
+  }
+
+
   render() {
     return (
-      <p>foo</p>
-    );
+      <div>
+        <h2>Puhelinluettelo</h2>
+
+        <form onSubmit={this.addContact}>
+          <div>
+            nimi: <input />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+
+        <h2>Numerot</h2>
+        <Rows rowData={this.state.persons} />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
