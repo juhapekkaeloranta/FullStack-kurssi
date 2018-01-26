e5c208862afedc05717b24515363603dc001d7ec
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-01-26 10:13:38 +0200

    tulostajaGeneraattori

--- Osa_2/two-five-phonebook/src/components/Rows.js
+++ Osa_2/two-five-phonebook/src/components/Rows.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { cloneElement } from 'react'
 import Row from './Row'
 
 const Rows = ({ rowsData, filterWord }) => {
@@ -7,6 +7,30 @@ const Rows = ({ rowsData, filterWord }) => {
     /** Ruma riippuvuus ulospäin! */
     return props.name.toLowerCase().includes(filterWord.toLowerCase())
   }
+
+  /** tulostajaGeneraattori on funktio, joka palauttaa funktion.
+   * Sille annetaan parametrinä string jota halutaan tulostella
+   */
+  const tulostajaGeneraattori = (tulostettavaSana) => {
+    return () => {
+      console.log(tulostettavaSana);
+    }
+  }
+
+  /** Tässä alustetaan kaksi tulostajaFunktiota generaattorin avulla
+   * Toinen tulostaa 'foo' ja toinen 'bar'
+   */
+  const fooTulostajaFunktio = tulostajaGeneraattori('foo')
+  const barTulostajaFunktio = tulostajaGeneraattori('bar')
+  
+  fooTulostajaFunktio() //sama kuin console.log('foo')
+  barTulostajaFunktio() //sama kuin console.log('bar')
+
+  /** Jos tulostaa itse tulostajaFunktion niin foo tai bar ei kuitenkaan näy.
+   * Seuraavat tulostavat siis täysin saman asian:
+   */
+  console.log(fooTulostajaFunktio)
+  console.log(barTulostajaFunktio)
 
   const filteredNames = rowsData.filter(containsSubstring)
 
