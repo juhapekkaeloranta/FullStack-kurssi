7e4b49ee681f199ad25f4c5ee50fd71d3c9478c1
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-01-28 23:26:32 +0200

    teht 2-12 done (with flags etc)

--- Osa_2/two-twelve-countries/src/components/Rows.js
+++ Osa_2/two-twelve-countries/src/components/Rows.js
@@ -21,13 +21,24 @@ const Rows = ({ rowsData, filteringTerm }) => {
 
   const filteredRows = rowsData.filter(countryFilter)
 
-  if (filteredRows.length > 10) {
-    console.log('liikaa!')
+  if (filteredRows.length === 1) {
+    console.log('showing just one!')
+    const country = filteredRows[0]
+    return (
+      <div>
+        <h1>{country.name} {country.nativeName}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <img src={country.flag} alt='Flag' width='150'></img>
+      </div>
+    )
+  } else if (filteredRows.length > 10) {
+    console.log('too many to show!')
     return (
       <p>too many results!</p>
     )
   } else {
-    console.log('näytetään!')
+    console.log('showing 1-10!')
     return (
       <ul>
         {filteredRows.map(row =>
