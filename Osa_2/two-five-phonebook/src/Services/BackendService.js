7d378b80f0f41a4ae6e7dc031079c229b48f6100
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-03 12:02:53 +0200

    Use heroku backend

--- Osa_2/two-five-phonebook/src/Services/BackendService.js
+++ Osa_2/two-five-phonebook/src/Services/BackendService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://boiling-waters-88355.herokuapp.com/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
